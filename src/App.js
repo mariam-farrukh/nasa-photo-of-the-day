738732ec575658fa7fc6628e86b2750e6686ba0c
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-07-17 16:06:56 -0500

    Fixed error, and now working on rendering image

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from 'axios';
 import Nasa from "./photo.js";
 
 function App() {
+  const[photo, setPhoto] = useState('');
+
+  useEffect(() => {
+      fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2019-07-17`)
+      .then(res => res.json())
+      .then(res => {
+          console.log("The res is working!", res);
+          setPhoto(res);
+      })
+      .catch( res => console.log("Error with useEffect axios", res));
+  }, []);
   return (
+
     <div className="App">
-      <Nasa/>
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+      <div>
+        <h1>{photo.title}</h1>
+        <img src={photo.url}></img>
+      </div>
     </div>
   );
 }
